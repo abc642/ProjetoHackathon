c2f9e77cf76468cb60f1d50553f8711e669a7f08
Benjamim <benjamim.francisco@hotmail.com>
2020-07-24 13:05:21 -0300

    pausa pro almoco

--- element/urls.py
+++ element/urls.py
@@ -30,4 +30,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home_page.urls')),
     url(r'^', include(router.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

f2bab07df3a2882986a2662228ef81bd3afaf13e
Benjamim <benjamim.francisco@hotmail.com>
2020-07-24 20:44:55 -0300

    tratado endpoint

--- element/settings.py
+++ element/settings.py
@@ -82,9 +82,7 @@ WSGI_APPLICATION = 'element.wsgi.application'
 
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR,'db.sqlite3')
 
-DATABASES = {
-    'default': config('DATABASE_URL',default=default_dburl,cast=dburl),
-}
+DATABASES = {'default': config('DATABASE_URL',default=default_dburl,cast=dburl),}
 
 
 # Password validation
--- element/urls.py
+++ element/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('',include('home_page.urls')),
-    url(r'^', include(router.urls)),
+    path('', include(router.urls)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
